4e99d51866d15cc511311dd4b9b7d0ea593ae499
roman thapa <thaparoman970@gmail.com>
2024-03-30 21:31:15 +0545

    brand wroking code

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Provider } from 'react-redux'
-import {store} from './app/store'
+import {store} from './reduxstore/store'
 import { PersistGate } from 'redux-persist/integration/react';
 import {persistStore} from "redux-persist";
 let persitor=persistStore(store);
