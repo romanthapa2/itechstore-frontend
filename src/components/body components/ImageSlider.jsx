591c33e6a9f21214d5219461c0fd7831cdd73af5
roman thapa <thaparoman970@gmail.com>
2024-06-17 20:35:56 +0545

    update : image in home

--- src/components/body components/ImageSlider.jsx
+++ src/components/body components/ImageSlider.jsx
@@ -4,8 +4,11 @@ import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
 const collection = [
   {
-    img: require("../../pictureskobau/pictures/macbook_air_m1__dxctrdvvxm4i_large.jpg"),
+    img : require("../../pictureskobau/pictures/banner_2x1.jpg")
   },
+  {
+    img : require("../../pictureskobau/pictures/legion-5i-pro-g8_2x1.png")
+  }
 ];
 
 const Imageslider = () => {
