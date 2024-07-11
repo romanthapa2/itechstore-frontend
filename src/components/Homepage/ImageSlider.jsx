2db5afdc17c615219f598ac6c42422c1282bcc12
roman thapa <thaparoman970@gmail.com>
2024-07-11 13:47:07 +0545

    update : change of imageslider component

--- src/components/Homepage/ImageSlider.jsx
+++ src/components/Homepage/ImageSlider.jsx
@@ -1,56 +1,124 @@
-import React, { useState } from "react";
-import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faAngleLeft, faAngleRight } from "@fortawesome/free-solid-svg-icons";
 
-const MemoizeFontAwesomeIcon = React.memo(FontAwesomeIcon)
 
-const collection = [
+import React from "react";
+import Slider from "react-slick";
+import Image1 from "../../pictures/61insmaQR-S._AC_SL1500_-removebg.png";
+import Image2 from "../../pictures/macbook.png";
+import Image4 from "../../pictures/images__2_-removebg-preview.png"
+// import Button from "../../utils/Button";
+
+const HeroData = [
   {
-    img : require("../../pictures/banner_2x1.jpg")
+    id: 1,
+    img: Image1,
+    subtitle: "Screen Touch",
+    title: "Lenovo",
+    title2: "360",
   },
   {
-    img : require("../../pictures/legion-5i-pro-g8_2x1.png")
-  }
+    id: 2,
+    img: Image2,
+    subtitle: "Fast",
+    title: "Apple",
+    title2: "M2 Chip",
+  },
+  {
+    id: 3,
+    img: Image4,
+    // subtitle: "Offer",
+    title: "Only R.s 2,00,000",
+    title2: "Offer",
+  },
+
 ];
 
 const Imageslider = () => {
-  const [current, setCurrent] = useState(0);
-  const length = collection.length;
-
-
-  const nextSlide = () => {
-    setCurrent(current === length - 1 ? 0 : current + 1);
+  const settings = {
+    dots: true,
+    arrows: false,
+    infinite: true,
+    speed: 800,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 4000,
+    cssEase: "ease-in-out",
+    pauseOnHover: false,
+    pauseOnFocus: true,
   };
-
-  const prevSlide = () => {
-    setCurrent(current === 0 ? length - 1 : current - 1);
-  };
-
   return (
-    <section className=" slider flex items-center justify-center relative mt-10  ml-9 mr-9">
-      <MemoizeFontAwesomeIcon
-        icon={faAngleLeft}
-        className="left-arrow size-10 absolute left-0 md-left-7 hover:text-white"
-        onClick={prevSlide}
-      />
-
-      {collection.map(({ img}, index) => {
-        return (
-          <div
-            className={`slide ${
-              index === current ? "active" : ""
-            } transition-opacity duration-500 ease-in-out`}
-            key={index}>
-            {index === current && <img src={img} alt="" className=" rounded-2xl" />}
-          </div>
-        );
-      })}
-      <MemoizeFontAwesomeIcon
-        icon={faAngleRight}
-        className="right-arrow size-10 absolute right-0 hover:text-white md-8"
-        onClick={nextSlide}
-      />
-    </section>
+    <div className="m-16">
+      <div
+        className="overflow-hidden rounded-3xl min-h-[450px] sm:min-h-[650px] bg-black flex justify-center items-center
+"
+      >
+        <div className="container pb-8 sm:pb-0">
+          {/* Hero section */}
+          <Slider {...settings}>
+            {HeroData.map((data) => (
+               
+              <div key={data.id}>
+                <div className="grid grid-cols-1 sm:grid-cols-2">
+                  {/* text content section */}
+                  <div className="flex flex-col justify-center gap-4 sm:pl-3 pt-12 sm:pt-0 text-center sm:text-left order-2 sm:order-1 relative z-10 ">
+                    <h1
+                      data-aos="zoom-out"
+                      data-aos-duration="500"
+                      data-aos-once="true"
+                      className="text-2xl sm:text-6xl lg:text-2xl font-bold"
+                    >
+                      {data.subtitle}
+                    </h1>
+                    <h1
+                      data-aos="zoom-out"
+                      data-aos-duration="500"
+                      data-aos-once="true"
+                      className="text-5xl sm:text-6xl lg:text-7xl font-bold"
+                    >
+                      {data?.title}
+                    </h1>
+                    <h1
+                      data-aos="zoom-out"
+                      data-aos-duration="500"
+                      data-aos-once="true"
+                      className="text-5xl uppercase text-white dark:text-white/5 sm:text-[80px] md:text-[100px] xl:text-[150px] font-bold"
+                    >
+                      {data.title2}
+                    </h1>
+                    {/* <div
+                      data-aos="fade-up"
+                      data-aos-offset="0"
+                      data-aos-duration="500"
+                      data-aos-delay="300"
+                    >
+                      <Button
+                        text="Shop By Category"
+                        bgColor="bg-primary"
+                        textColor="text-white"
+                        // handler={handleOrderPopup}
+                      />
+                    </div> */}
+                  </div>
+                  {/* Img section */}
+                  <div className="order-1 sm:order-2">
+                    <div
+                      data-aos="zoom-in"
+                      data-aos-once="true"
+                      className="relative z-10"
+                    >
+                      <img
+                        src={data.img}
+                        alt="image"
+                        className="w-[300px] sm:w-[450px] h-[300px] sm:h-[450px] sm:scale-105 lg:scale-120 object-contain mx-auto drop-shadow-[-8px_4px_6px_rgba(0,0,0,.4)] relative z-40"
+                      />
+                    </div>
+                  </div>
+                </div>
+              </div>
+            ))}
+          </Slider>
+        </div>
+      </div>
+    </div>
   );
 };
 
--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ import { Provider } from "react-redux";
 import { store } from "./reduxstore/store";
 import { PersistGate } from "redux-persist/integration/react";
 import { persistStore } from "redux-persist";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
 let persitor = persistStore(store);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
