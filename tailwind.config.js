251d10890ca7de56e06e3c7abefbd55bd72998b5
roman thapa <thaparoman970@gmail.com>
2024-07-27 19:27:08 +0545

    update : bug-fix in tailwind config due to folder

--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,7 @@ const { fontFamily } = require("tailwindcss/defaultTheme")
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   darkMode: ["class"],
-  content: ["app/**/*.{js,jsx}", "components/**/*.{js,jsx}"],
+  content: ["src/**/*.{js,jsx}", "components/**/*.{js,jsx}"],
   theme: {
     container: {
       center: true,
