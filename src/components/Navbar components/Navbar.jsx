d08b71da9f0f4f92aa2eb8ca611973ba0e814781
roman thapa <thaparoman970@gmail.com>
2024-07-28 21:43:04 +0545

    update : ui of sidebar in small screen

--- src/components/Navbar components/Navbar.jsx
+++ src/components/Navbar components/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
+import { useState } from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
-import { faBars, faMagnifyingGlass, faCartPlus } from "@fortawesome/free-solid-svg-icons";
+import { faBars, faMagnifyingGlass, faCartPlus , faXmark } from "@fortawesome/free-solid-svg-icons";
 import { Link } from "react-router-dom";
 import Types from "./Types";
 import Bands from "./Bands";
@@ -8,12 +9,17 @@ import Bands from "./Bands";
 const MemoizeFontAwesomeIcon = React.memo(FontAwesomeIcon);
 
 const Navbar = () => {
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
   return (
     <header className="flex md:px-16 px-6 justify-between bg-white h-14 items-center">
-      {/* bar icon and topic of the shop */}
-      <MemoizeFontAwesomeIcon icon={faBars} className="text-2xl md:ml-5 md:hidden" />
+      <MemoizeFontAwesomeIcon icon={faBars} onClick={toggleSidebar} className="text-2xl md:ml-5 md:hidden" />
       <div>
-        <h1 className="font-semibold text-2xl">ShopOnline</h1>
+        <h1 className="font-semibold text-2xl">itechStore</h1>
       </div>
 
       {/* different topics for navbar */}
@@ -39,6 +45,39 @@ const Navbar = () => {
         </Link>
         {/* <Link to="/admin">admin</Link> */}
       </div>
+
+      {sidebarOpen && (
+        <div className="fixed inset-0 flex z-50">
+          <div className="w-64 bg-white shadow-md p-10">
+            <ul className="flex flex-col">
+              <div className="flex justify-between ">
+              <Link to="/" className="mb-4 text-3xl">
+                Home
+              </Link>
+              <FontAwesomeIcon icon={faXmark} className="text-3xl" onClick={toggleSidebar} />
+              </div>
+              <div className="mb-4">
+                <h1 className="text-2xl mb-3 font-semibold">Categories</h1>
+                <ul className="space-y-3 text-2xl">
+                  <li>Laptops</li>
+                  <li>Monitors</li>
+                  <li>Desktops</li>
+                  <li>Show all</li>
+                </ul>
+              </div>
+              <div className="mb-4">
+                <h1 className="text-2xl mb-3 font-semibold">Brands</h1>
+                <ul className="space-y-3 text-2xl">
+                  <li>Apple</li>
+                  <li>Lenovo</li>
+                  <li>Dell</li>
+                  <li>Show all</li>
+                </ul>
+              </div>
+            </ul>
+          </div>
+        </div>
+      )}
     </header>
   );
 };
