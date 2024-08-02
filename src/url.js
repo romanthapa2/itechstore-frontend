628160fd0cb6dd6a45a283740a2184c0b2f424e7
roman thapa <thaparoman970@gmail.com>
2024-08-02 08:28:29 +0545

    url

--- src/url.js
+++ src/url.js
@@ -1,3 +1,3 @@
-// export const url = "https://shoponline-backend.onrender.com"
+export const url = "https://shoponline-backend.onrender.com"
 
-export const url = "http://localhost:5000"
+// export const url = "http://localhost:5000"
